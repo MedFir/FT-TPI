8d706d312a123ab2384729a4f0bbeb330efe1c7d
MedFir <ezequielbysuarez@gmail.com>
2026-09-22 20:54:46 -0300

    se agregaron 2 funciones mas y se hicieron 4 test mas

--- src/funciones/funciones.js
+++ src/funciones/funciones.js
@@ -1,3 +1,14 @@
 export function sumar(a, b) {
   return a + b;
 }
+
+export function esMayorDeEdad(edad) {
+  if (edad >= 18) {
+    return true;
+  }
+  return false
+}
+
+export function saludar(nombre) {
+  return `Hola, ${nombre}!`;
+}
--- src/funciones/funciones.test.js
+++ src/funciones/funciones.test.js
@@ -1,10 +1,38 @@
-import {sumar} from "./funciones.js"
+import { sumar } from "./funciones.js"
+import {esMayorDeEdad} from "./funciones.js"
+import {saludar} from "./funciones.js"
+
 import { describe, it, test, expect } from "vitest";
 
-test("devuelve 9 cuando suma 5 y 4", () => {
+test("el total es 8", () => {
   const a = 5;
-  const b = 4;
+  const b = 3;
   const res = sumar(a, b);
 
-  expect(res).toBe(9);
+  expect(res).toBe(8);
+});
+test("el numero es 0", () => {
+  const a = 0;
+  const res = sumar(a, a);
+  expect(res).toBe(0);
+});
+
+
+test("edades 20 y true", () => {
+  const edad = 20;
+  const res = esMayorDeEdad(edad);
+
+  expect(res).toBe(true);
+});
+test("edades 17 y false", () => {
+  const edad = 17;
+  const res = esMayorDeEdad(edad);
+
+  expect(res).toBe(false);
+});
+test("texto correcto= Hola, Ana!", () => {
+  const nom = "Ana";
+  const res = saludar(nom);
+
+  expect(res).toBe("Hola, Ana!");
 });
